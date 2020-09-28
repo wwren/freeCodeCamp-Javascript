eb108f8403a4e2c5a0baacfff6cfa25c3f6a3394
ran ding <65450330+wwren@users.noreply.github.com>
2020-09-28 22:18:35 +1000

    es6

---
+++ es6/21. use-getters-and-setters-to-control-access-to-an-object.js
@@ -0,0 +1,21 @@
+// Only change code below this line
+class Thermostat {
+  constructor(temp) {
+    this._temp = temp;
+  }
+
+  get temperature(){
+    return 5/9 * (this._temp - 32);
+  }
+
+  set temperature(updateTemp) {
+    this._temp = updateTemp * 9.0 / 5 + 32; 
+  }
+
+}
+// Only change code above this line
+
+const thermos = new Thermostat(76); // Setting in Fahrenheit scale
+let temp = thermos.temperature; // 24.44 in Celsius
+thermos.temperature = 26;
+temp = thermos.temperature; // 26 in Celsius
---
+++ es6/31. handle-a-rejected-promise-with-catch.js
@@ -0,0 +1,18 @@
+const makeServerRequest = new Promise((resolve, reject) => {
+  // responseFromServer is set to false to represent an unsuccessful response from a server
+  let responseFromServer = false;
+    
+  if(responseFromServer) {
+    resolve("We got the data");
+  } else {  
+    reject("Data not received");
+    makeServerRequest.catch(error => {"Data not received"});
+    console.log(error); 
+  }
+});
+
+makeServerRequest.then(result => {
+  console.log(result);
+});
+
+
